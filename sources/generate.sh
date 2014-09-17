2af84a90996dfa0d369fac0d0b960f080b398a10
Thanatermesis <thanatermesis@gmail.com>
2014-09-17 22:32:03 +0200

    gdm added too

---
+++ sources/generate.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+rm -f *png
+
+rsvg --format png --height 480 --width 640 elive-wallpaper-generic-grub.svg elive-wallpaper-generic-grub.png
+
+
+#rsvg --format png --height 1920 --width 1080 gdm.svg elive-wallpaper-generic-gdm.png
