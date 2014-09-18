6a6b55e7215fbbfddae5ed70d86e6840ac6e448c
Thanatermesis <thanatermesis@gmail.com>
2014-09-18 02:58:49 +0200

    improved grub wallpaper for common new widescreens

--- sources/generate.sh
+++ sources/generate.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-rm -f *png
+set -x
 
-rsvg --format png --height 480 --width 640 elive-wallpaper-generic-grub.svg elive-wallpaper-generic-grub.png
+rsvg --format png --height 480 --width 640 elive-wallpaper-generic-grub.svg ../tree/usr/share/images/desktop-base/elive-wallpaper-generic-grub.png
 
 
 #rsvg --format png --height 1920 --width 1080 gdm.svg elive-wallpaper-generic-gdm.png
