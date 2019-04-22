abfa1eb562fc8b01271871fd752a8470c877a6ab
Thanatermesis <thanatermesis@gmail.com>
2019-04-22 23:15:12 +0200

    removed for new sources version

--- sources/generate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -x
-
-rsvg --format png --height 480 --width 640 elive-wallpaper-generic-grub.svg ../tree/usr/share/images/desktop-base/elive-wallpaper-generic-grub.png
-
-
-#rsvg --format png --height 1920 --width 1080 gdm.svg elive-wallpaper-generic-gdm.png
